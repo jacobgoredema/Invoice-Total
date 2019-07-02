1f7d2c0d4ffeec4a5341a594d8bbb82e6764c62e
Jacob <jacobgoredema@gmail.com>
2019-07-02 07:54:57 +0200

    added code behind

--- InvoiceTotal/frmInvoiceTotal.Designer.cs
+++ InvoiceTotal/frmInvoiceTotal.Designer.cs
@@ -55,7 +55,7 @@
             this.label2.Location = new System.Drawing.Point(24, 66);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(110, 13);
-            this.label2.TabIndex = 1;
+            this.label2.TabIndex = 2;
             this.label2.Text = "Discount Percentage:";
             // 
             // label3
@@ -64,7 +64,7 @@
             this.label3.Location = new System.Drawing.Point(24, 94);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(91, 13);
-            this.label3.TabIndex = 2;
+            this.label3.TabIndex = 4;
             this.label3.Text = "Discount Amount:";
             // 
             // label4
@@ -73,7 +73,7 @@
             this.label4.Location = new System.Drawing.Point(24, 120);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(34, 13);
-            this.label4.TabIndex = 3;
+            this.label4.TabIndex = 6;
             this.label4.Text = "Total:";
             // 
             // btnCalculate
@@ -81,16 +81,17 @@
             this.btnCalculate.Location = new System.Drawing.Point(79, 169);
             this.btnCalculate.Name = "btnCalculate";
             this.btnCalculate.Size = new System.Drawing.Size(75, 23);
-            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.TabIndex = 8;
             this.btnCalculate.Text = "Calculate";
             this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
             // 
             // btnExit
             // 
             this.btnExit.Location = new System.Drawing.Point(167, 169);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 23);
-            this.btnExit.TabIndex = 5;
+            this.btnExit.TabIndex = 9;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
@@ -100,28 +101,28 @@
             this.txtSubTotal.Location = new System.Drawing.Point(137, 36);
             this.txtSubTotal.Name = "txtSubTotal";
             this.txtSubTotal.Size = new System.Drawing.Size(135, 20);
-            this.txtSubTotal.TabIndex = 7;
+            this.txtSubTotal.TabIndex = 1;
             // 
             // txtDiscountPercentage
             // 
             this.txtDiscountPercentage.Location = new System.Drawing.Point(137, 63);
             this.txtDiscountPercentage.Name = "txtDiscountPercentage";
             this.txtDiscountPercentage.Size = new System.Drawing.Size(135, 20);
-            this.txtDiscountPercentage.TabIndex = 8;
+            this.txtDiscountPercentage.TabIndex = 3;
             // 
             // txtDiscountAmount
             // 
             this.txtDiscountAmount.Location = new System.Drawing.Point(137, 91);
             this.txtDiscountAmount.Name = "txtDiscountAmount";
             this.txtDiscountAmount.Size = new System.Drawing.Size(135, 20);
-            this.txtDiscountAmount.TabIndex = 9;
+            this.txtDiscountAmount.TabIndex = 5;
             // 
             // txtTotal
             // 
             this.txtTotal.Location = new System.Drawing.Point(137, 117);
             this.txtTotal.Name = "txtTotal";
             this.txtTotal.Size = new System.Drawing.Size(135, 20);
-            this.txtTotal.TabIndex = 10;
+            this.txtTotal.TabIndex = 7;
             // 
             // frmInvoiceTotal
             // 
--- InvoiceTotal/frmInvoiceTotal.cs
+++ InvoiceTotal/frmInvoiceTotal.cs
@@ -21,5 +21,36 @@ namespace InvoiceTotal
         {
             this.Close();
         }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            decimal subtotal = Convert.ToDecimal(txtSubTotal.Text);
+            decimal discountPercent = 0m;
+            decimal discountAmount = 0m;
+            decimal invoiceTotal = 0m;
+
+            if (subtotal>=500)
+            {
+                discountPercent = .2m;
+            }
+            else if(subtotal>=250 && subtotal < 500)
+            {
+                discountPercent = .15m;
+            }
+            else if(subtotal>=100 && subtotal < 250)
+            {
+                discountPercent = .1m;
+            }
+
+
+            discountAmount = subtotal * discountPercent;
+            invoiceTotal = subtotal - discountAmount;
+
+            txtDiscountPercentage.Text = discountPercent.ToString("p1");
+            txtDiscountAmount.Text = discountAmount.ToString("c");
+            txtTotal.Text = invoiceTotal.ToString("c");
+
+            txtSubTotal.Focus();
+        }
     }
 }
