b894af17b55a9699ba1cb99b2169bf88af737522
Jacob <jacobgoredema@gmail.com>
2019-07-03 17:18:15 +0200

    customer Type

---
+++ InvoiceTotal/CustomerType.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InvoiceTotal
+{
+    public partial class CustomerType : Form
+    {
+        public CustomerType()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            string customerType = txtCustomerType.Text;
+            decimal subtotal = Convert.ToDecimal(txtSubtotal.Text);
+            decimal discountPercentage = .0m;
+
+            if (true)
+            {
+
+            }
+            else if (true)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            decimal discountAmount = subtotal * discountPercentage;
+            decimal invoiceTotal = subtotal - discountAmount;
+
+            txtDiscountPercentage.Text = discountPercentage.ToString("p1");
+            txtDiscountAmount.Text = discountAmount.ToString("c");
+            txtTotal.Text = invoiceTotal.ToString("c");
+
+            txtCustomerType.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
